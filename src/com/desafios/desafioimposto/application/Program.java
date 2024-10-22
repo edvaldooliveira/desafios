40847613bcf8b9fd18b37317aeff8edf962238bc
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-22 20:53:24 +0100

    Calculo do imposto

--- src/com/desafios/desafioimposto/application/Program.java
+++ src/com/desafios/desafioimposto/application/Program.java
@@ -1,8 +1,22 @@
 package com.desafios.desafioimposto.application;
 
+import com.desafios.desafioimposto.entities.CalcImposto;
+
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        Scanner sc = new Scanner(System.in);
+
+        CalcImposto calcImposto = new CalcImposto();
+
+
+        System.out.print("Renda anual com salário: ");
+        double salario = sc.nextDouble();
+
+        //System.out.println(calcImposto.impostoSalario(salario));
+       // System.out.println(calcImposto.impostoServicos(salario));
+        System.out.println(calcImposto.impostoCapital(salario));
 
     }
 }
--- src/com/desafios/desafioimposto/entities/CalcImposto.java
+++ src/com/desafios/desafioimposto/entities/CalcImposto.java
@@ -2,5 +2,18 @@ package com.desafios.desafioimposto.entities;
 
 public class CalcImposto {
 
+    //Calculo do imposto
+    public double  impostoSalario(double rendaSalario) {
+        return rendaSalario * 0.1;
+    }
+
+    public double  impostoServicos(double rendaServicos) {
+        return rendaServicos * 0.15;
+    }
+
+    public double impostoCapital(double ganhoCapital) {
+        return ganhoCapital * 0.2;
+    }
+
 
 }
