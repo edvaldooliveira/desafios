e49a1c89e08f1c90dcc0a7685ad832d4ee2c736b
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-24 20:59:46 +0100

    Início impl aplicativo

--- src/com/desafios/desafioimposto/application/Program.java
+++ src/com/desafios/desafioimposto/application/Program.java
@@ -3,24 +3,28 @@ package com.desafios.desafioimposto.application;
 import com.desafios.desafioimposto.entities.rendaMensal;
 import com.desafios.desafioimposto.entities.CalcSalario;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        rendaMensal calcImposto = new rendaMensal();
-        CalcSalario calcSalario = new CalcSalario();
-
-
         System.out.print("Renda anual com salário: ");
-        double salario = sc.nextDouble();
+        double salário = sc.nextDouble();
+        System.out.print("Renda anual com prestação de serviço: ");
+        double pestServico = sc.nextDouble();
+        System.out.print("Renda anual com ganho de capital: ");
+        double ganhoCapital = sc.nextDouble();
+        System.out.print("Gastos médicos: ");
+        double GastosMedio = sc.nextDouble();
+        System.out.print("Gastos educacionais: ");
+        double GastosEducacionais = sc.nextDouble();
 
-        //System.out.println(calcImposto.impostoSalario(salario));
-       // System.out.println(calcImposto.impostoServicos(salario));
-        //System.out.println(calcImposto.impostoCapital(salario));
-       double reslt = calcSalario.calcularImposto(salario);
-        System.out.println(reslt);
+        sc.close();
+
+
 
     }
 }
