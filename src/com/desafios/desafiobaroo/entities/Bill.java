0c8026f805e3b8aa7d58174fff0a5a073b25e30b
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-31 21:17:54 +0000

    Inicio desafiobaroo

---
+++ src/com/desafios/desafiobaroo/entities/Bill.java
@@ -0,0 +1,78 @@
+package com.desafios.desafiobaroo.entities;
+
+public class Bill {
+
+    private char gender;
+    private int beer;
+    private int barbecue;
+    private int sofDrink;
+
+
+    public Bill() {
+    }
+
+    public Bill(char gender, int beer, int barbecue, int sofDrink) {
+        this.gender = gender;
+        this.beer = beer;
+        this.barbecue = barbecue;
+        this.sofDrink = sofDrink;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getBeer() {
+        return beer;
+    }
+
+    public void setBeer(int beer) {
+        this.beer = beer;
+    }
+
+    public int getBarbecue() {
+        return barbecue;
+    }
+
+    public void setBarbecue(int barbecue) {
+        this.barbecue = barbecue;
+    }
+
+    public int getSofDrink() {
+        return sofDrink;
+    }
+
+    public void setSofDrink(int sofDrink) {
+        this.sofDrink = sofDrink;
+    }
+
+    public double cover() {
+        return 0;
+    }
+
+    public double feeding() {
+        return 0;
+    }
+
+    public double tichet() {
+        return 0;
+    }
+
+    public double total() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "gender=" + gender +
+                ", beer=" + beer +
+                ", barbecue=" + barbecue +
+                ", sofDrink=" + sofDrink +
+                '}';
+    }
+}
