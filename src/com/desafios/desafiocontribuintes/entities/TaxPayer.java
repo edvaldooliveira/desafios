cf0a8af14cce1394654f7324d038f27487d58388
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-11-01 22:00:46 +0000

    Construtores, geters, seters

--- src/com/desafios/desafiocontribuintes/entities/TaxPayer.java
+++ src/com/desafios/desafiocontribuintes/entities/TaxPayer.java
@@ -7,6 +7,59 @@ public class TaxPayer {
     private Double healthSpending;
     private Double educationSpending;
 
+    public TaxPayer() {
+    }
+
+    public TaxPayer(Double salaryIncome, Double servivesIncome, Double capitalIncome, Double healthSpending, Double educationSpending) {
+        this.salaryIncome = salaryIncome;
+        this.servivesIncome = servivesIncome;
+        this.capitalIncome = capitalIncome;
+        this.healthSpending = healthSpending;
+        this.educationSpending = educationSpending;
+    }
+
+    public Double getSalaryIncome() {
+        return salaryIncome;
+    }
+
+    public void setSalaryIncome(Double salaryIncome) {
+        this.salaryIncome = salaryIncome;
+    }
+
+    public Double getServivesIncome() {
+        return servivesIncome;
+    }
+
+    public void setServivesIncome(Double servivesIncome) {
+        this.servivesIncome = servivesIncome;
+    }
+
+    public Double getCapitalIncome() {
+        return capitalIncome;
+    }
+
+    public void setCapitalIncome(Double capitalIncome) {
+        this.capitalIncome = capitalIncome;
+    }
+
+    public Double getHealthSpending() {
+        return healthSpending;
+    }
+
+    public void setHealthSpending(Double healthSpending) {
+        this.healthSpending = healthSpending;
+    }
+
+    public Double getEducationSpending() {
+        return educationSpending;
+    }
+
+    public void setEducationSpending(Double educationSpending) {
+        this.educationSpending = educationSpending;
+    }
+
+    
+    //Métodos para serem implementados
     public double salaryTax() {
         return 0;
     }
