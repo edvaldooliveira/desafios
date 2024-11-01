c8523ad1d0fc23ccb9e2a369856bc955237cbfa8
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-11-01 21:35:56 +0000

    Desafio combate finalizado

--- src/com/desafios/desafiocombate/application/Program.java
+++ src/com/desafios/desafiocombate/application/Program.java
@@ -10,7 +10,8 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite os dados do primeiro campeão:");
+
+        System.out.println("\nDigite os dados do primeiro campeão:");
         System.out.print("Nome: ");
         String name1 = sc.nextLine();
         System.out.print("Vida inicial: ");
@@ -21,8 +22,7 @@ public class Program {
         int armor1 = sc.nextInt();
         sc.nextLine();
 
-        Champion champion = new Champion(name1, life1, attack1, armor1);
-        System.out.println(champion);
+        Champion champion1 = new Champion(name1, life1, attack1, armor1);
 
         System.out.println("Digite os dados do segundo campeão:");
         System.out.print("Nome: ");
@@ -34,15 +34,30 @@ public class Program {
         System.out.print("Armadura: ");
         int armor2 = sc.nextInt();
 
-
         Champion champion2 = new Champion(name2, life2, attack2, armor2);
-        System.out.println(champion2);
+
+        System.out.println("\n---------");
+
+        System.out.print("\nQuantos turnos você deseja executar? ");
+        int turnos = sc.nextInt();
 
 
+        for (int i = 1; i <= turnos; i++) {
+            System.out.printf("Resultado do turno %d:\n", i);
+            champion1.takeDamage(champion2);
+            champion2.takeDamage(champion1);
 
+            System.out.println(champion1.status());
+            System.out.println(champion2.status()+"\n");
 
+            System.out.println("---------");
 
+            if (champion1.getLife() == 0 || champion2.getLife() == 0) {
+                break;
+            }
+        }
 
+        System.out.println("FIM DO COMBATE");
         sc.close();
+        }
     }
-}
--- src/com/desafios/desafiocombate/entities/Champion.java
+++ src/com/desafios/desafiocombate/entities/Champion.java
@@ -8,58 +8,45 @@ public class Champion {
 
     public Champion() {
     }
-
     public Champion(String name, int life, int attack, int armor) {
         this.name = name;
         this.life = life;
         this.attack = attack;
         this.armor = armor;
     }
-
-    // Getters e Setters
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getLife() {
         return life;
     }
-
     public void setLife(int life) {
         this.life = life;
     }
-
     public int getAttack() {
         return attack;
     }
-
     public void setAttack(int attack) {
         this.attack = attack;
     }
-
     public int getArmor() {
         return armor;
     }
-
     public void setArmor(int armor) {
         this.armor = armor;
     }
 
-    // Método para calcular dano recebido
     public void takeDamage(Champion opponent) {
-        int damage = Math.max(opponent.getAttack() - this.armor, 1); // Garante dano mínimo de 1
-        this.life = Math.max(this.life - damage, 0); // Garante que a vida não fique negativa
+        int damage = Math.max(opponent.getAttack() - this.armor, 1);
+        this.life = Math.max(this.life - damage, 0); 
     }
 
-    // Método para exibir o status atual do campeão
     public String status() {
         return this.name + ": " + this.life + " de vida";
     }
-
     @Override
     public String toString() {
         return "Champion{" +
