df2fa2cf3ff804cf19e12793c57492deb8af41d2
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-11-01 16:56:14 +0000

    Agusto no desafioimposto

--- src/com/desafios/desafioimposto/application/Program.java
+++ src/com/desafios/desafioimposto/application/Program.java
@@ -5,13 +5,11 @@ import com.desafios.desafioimposto.entities.RendaAnualCalculadora;
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
-        RendaAnualCalculadora calculadora = new RendaAnualCalculadora();
-
 
         // Entrada de dados
         System.out.print("\nRenda anual com salário: ");
@@ -26,14 +24,17 @@ public class Program {
         System.out.print("Gastos educacionais: ");
         double gastosEducacionais = sc.nextDouble();
 
-        // Cálculos de impostos
+        // Criação do objeto calculadora
+        RendaAnualCalculadora calculadora = new RendaAnualCalculadora(rendaAnualSalario, rendaAnualServico, rendaAnualCapital);
+
+        // Cálculo dos impostos
         double impostoSalario = calculadora.impostoSalario(rendaAnualSalario);
         double impostoServico = calculadora.impostoServico(rendaAnualServico);
         double impostoCapital = calculadora.impostoCapital(rendaAnualCapital);
 
         double impostoBrutoTotal = impostoSalario + impostoServico + impostoCapital;
 
-        // Cálculo de deduções
+        // Cálculo das deduções
         double maximoDedutivel = impostoBrutoTotal * 0.3;
         double gastosDedutiveis = Math.min(gastosMedicos + gastosEducacionais, maximoDedutivel);
         double gDedutiveis =  gastosEducacionais + gastosMedicos;
@@ -44,12 +45,11 @@ public class Program {
         System.out.println("CONSOLIDADO DE RENDA:");
         System.out.printf("Imposto sobre salário: %.2f\n", impostoSalario);
         System.out.printf("Imposto sobre serviços: %.2f\n", impostoServico);
-        System.out.printf("Imposto sobre ganho de capital: %.2f\n", impostoCapital);
+        System.out.printf("Imposto sobre ganho de capital: %.2f\n",  impostoCapital);
 
         System.out.println("\nDEDUÇÕES:");
         System.out.printf("Máximo dedutível: %.2f\n", maximoDedutivel);
         System.out.printf("Gastos dedutíveis: %.2f\n", gDedutiveis);
-
 
         System.out.println("\nRESUMO:");
         System.out.printf("Imposto bruto total: %.2f\n", impostoBrutoTotal);
@@ -58,4 +58,5 @@ public class Program {
 
         sc.close();
     }
+
 }
--- src/com/desafios/desafioimposto/entities/RendaAnualCalculadora.java
+++ src/com/desafios/desafioimposto/entities/RendaAnualCalculadora.java
@@ -2,6 +2,36 @@ package com.desafios.desafioimposto.entities;
 
 public class RendaAnualCalculadora {
 
+    private double rendaAnualSalario;
+    private double impostoServico;
+    private double impostoCapital;
+
+    public RendaAnualCalculadora() {
+    }
+    public RendaAnualCalculadora(double rendaAnualSalario, double impostoServico, double impostoCapital) {
+        this.rendaAnualSalario = rendaAnualSalario;
+        this.impostoServico = impostoServico;
+        this.impostoCapital = impostoCapital;
+    }
+
+    public double getRendaAnualSalario() {
+        return rendaAnualSalario;
+    }
+    public void setRendaAnualSalario(double rendaAnualSalario) {
+        this.rendaAnualSalario = rendaAnualSalario;
+    }
+    public double getImpostoServico() {
+        return impostoServico;
+    }
+    public void setImpostoServico(double impostoServico) {
+        this.impostoServico = impostoServico;
+    }
+    public double getImpostoCapital() {
+        return impostoCapital;
+    }
+    public void setImpostoCapital(double impostoCapital) {
+        this.impostoCapital = impostoCapital;
+    }
     public double impostoSalario(double rendaAnualSalario) {
         double rendaMensal = rendaAnualSalario / 12;
         if (rendaMensal < 3000) {
@@ -12,12 +42,14 @@ public class RendaAnualCalculadora {
             return rendaAnualSalario * 0.2;
         }
     }
-
     public double impostoServico(double rendaAnualServico) {
         return rendaAnualServico * 0.15;
     }
-
     public double impostoCapital(double rendaAnualCapital) {
         return rendaAnualCapital * 0.2;
     }
 }
+
+
+
+
